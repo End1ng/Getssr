9cac3bd171b40974dcbba28d9b8c70bd999d29ab
Y <y>
2017-07-01 23:36:09 +0800

    添加非root提醒

--- getSs.py
+++ getSs.py
@@ -6,14 +6,18 @@ import requests, re, sys, os, signal
 url = "https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
 comp = re.compile("<p>服务器\d*(.*?)：([\w\.]+)\s*端口.*?(\d*)\s*密码.*?([\w\.-]*)\s*加密方式：([\w-]*)")
 
+if os.geteuid() != 0:
+    sys.exit("需要root权限： sudo python getss.py")
+
 def CtrlCHandler(signum, frame):
-    sys.exit(u"退出程序")
+    sys.exit("再见")
 signal.signal(signal.SIGINT, CtrlCHandler)
 
 try:
+    print "获取中..."
     r = requests.get(url)
 except:
-    sys.exit(u"网络错误")
+    sys.exit("网络错误")
 
 server_list = {}
 count = 1
@@ -29,7 +33,7 @@ id = raw_input("请输入id:")
 while not id.isdigit() or int(id) not in server_list:
     id = raw_input("请输入正确的id:")
 id = int(id)
-c = "sslocal -s %s -p %s -k %s -m %s -d restart -q > /dev/null" % (server_list[id]['i'], server_list[id]['p'], server_list[id]['k'], server_list[id]['m'])
+c = "sslocal -s %s -p %s -k %s -m %s -d restart -q > /dev/null 2>&1" % (server_list[id]['i'], server_list[id]['p'], server_list[id]['k'], server_list[id]['m'])
 if os.system(c) == 0:
     print "已开启服务:%s:%s在%s" % (server_list[id]['i'], server_list[id]['p'], server_list[id]['s'])
 else:
