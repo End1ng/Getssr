88a8e4ccd562770bc0a030b476b01bbcc4046609
Y <Y>
2017-09-02 16:40:41 +0800

    斗志斗勇

--- getSs.py
+++ getSs.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# auther: ni7eip
+# auther: ni7eipr
 
-import requests, re, sys, os, signal, base64
+import requests, re, sys, os, signal, base64, json, time
 from bs4 import BeautifulSoup
 
 ssr_path = "/opt/shadowsocksr/shadowsocks/local.py"
@@ -19,60 +19,70 @@ def add_padding(data):
 
 def parse_ss(ss):
     ss = add_padding(ss).split(':')
+    # ipv4 ssr链接
+    if ss.__len__() == 6:
+        return {'s':ss[0],'p':ss[1],'O':ss[2],'m':ss[3],'o':ss[4],'k':add_padding(ss[5].split('/')[0])}
+    # ipv4 ss链接
     if ss.__len__() == 3:
         return {'s':ss[1].split('@')[1],'p':ss[2],'O':'origin','m':ss[0],'o':'plain','k':ss[1].split('@')[0]}
-    if ss.__len__() == 9:
-        return {'s':':'.join([ss[1].split('@')[1], ss[2],  ss[3], ss[4], ss[5], ss[6], ss[7]]),'p':ss[8],'O':'origin','m':ss[0],'o':'plain','k':ss[1].split('@')[0]}
-    if ss.__len__() == 11:
-        password = add_padding(ss[10].split('/')[0])
-        return {'s':':'.join([ss[0],ss[1],ss[2],ss[3],ss[4],ss[5]]),'p':ss[6],'O':ss[7],'m':ss[8],'o':ss[9],'k':password}
-    password = add_padding(ss[5].split('/')[0])
-    return {'s':ss[0],'p':ss[1],'O':ss[2],'m':ss[3],'o':ss[4],'k':password}
-print "获取中......"
+    # ipv6 链接
+    return {'s':':'.join(ss[:-5]),'p':ss[-5],'O':ss[-4],'m':ss[-3],'o':ss[-2],'k':add_padding(ss[-1].split('/')[0])}
 
-url = "https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
-# comp_ogete_ss = re.compile("<p>服务器\d*(.*?)：([\w\.]+)\s*端口.*?(\d*)\s*密码.*?([\w\.-]*)\s*加密方式.*?([\w-]*)")
-comp_ogete_ssr = re.compile("<p>服务器\d+.+?([\w\.]+)\s*端口.+?(\d*)\s*密码.+?([\w\.-]*)\s*加密方式.+?([\w\.-]*)\s*SSR协议.+?协议.+?([\w\.-]*)\s*混淆.+?([\w\.-]*)\s*\（自建\）.*")
-try:
-    res = requests.get(url).content
-except:
-    print "获取 " + url + " 失败"
-html = re.findall("<p>服务器.*</p>", res)
-for i in html:
-    # res = re.match(comp_ogete_ss, i)
-    # if res:
-    #     S[S.__len__()] = {'s':res.group(1), 'i':res.group(2), 'p':res.group(3), 'k':res.group(4), 'm':res.group(5)}
-    res = re.match(comp_ogete_ssr, i)
-    if res:
-        S[S.__len__()] = {'s':res.group(1),'p':res.group(2),'k':res.group(3),'m':res.group(4),'O':res.group(5),'o':res.group(6),'l':''}
+def Alvin9999():
+    url = "https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
+    # comp_ogete_ss = re.compile("<p>服务器\d*(.*?)：([\w\.]+)\s*端口.*?(\d*)\s*密码.*?([\w\.-]*)\s*加密方式.*?([\w-]*)")
+    comp_ogete_ssr = re.compile("<p>服务器\d+.+?([\w\.]+)\s*端口.+?(\d*)\s*密码.+?([\w\.-]*)\s*加密方式.+?([\w\.-]*)\s*SSR协议.+?协议.+?([\w\.-]*)\s*混淆.+?([\w\.-]*)\s*\（自建\）.*")
+    try:
+        res = requests.get(url).content
+    except:
+        print "获取 " + url + " 失败"
+    html = re.findall("<p>服务器.*</p>", res)
+    for i in html:
+        # res = re.match(comp_ogete_ss, i)
+        # if res:
+        #     S[S.__len__()] = {'s':res.group(1), 'i':res.group(2), 'p':res.group(3), 'k':res.group(4), 'm':res.group(5)}
+        res = re.match(comp_ogete_ssr, i)
+        if res:
+            S[S.__len__()] = {'s':res.group(1),'p':res.group(2),'k':res.group(3),'m':res.group(4),'O':res.group(5),'o':res.group(6),'l':''}
 
-url = "https://doub.bid/sszhfx"
-try:
-    session = requests.Session()
-    session.get(url)
-    session.post("https://doub.bid/wp-login.php?action=postpass", data={'post_password':'40'})
-    res = session.get(url).content
-    soup = BeautifulSoup(res, 'lxml')
-    for tr in soup.find_all('tr'):
-        td = tr.find_all('td')
-        if td.__len__() == 7:
-            id = S.__len__()
-            S[id] = parse_ss(td[6].find('a', "dl1")['href'].split('//')[2])
-            S[id]['l'] = td[0].text
-except:
-    print "获取 " + url + " 失败"
+def doub():
+    url = "https://doub.bid/sszhfx"
+    try:
+        session = requests.Session()
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0'
+        }
+        res = session.get(url, headers=headers)
+        print re.search('<p id="problem">(.*)</p>', res.text).group(1)
+        input_text = raw_input("请输入答案:")
+        session.post(re.search('<form action="(.*)" method="post">', res.text).group(1), data={'post_password':input_text}, headers=headers)
+        res = session.get(url,  headers=headers).content
+        soup = BeautifulSoup(res, 'lxml')
+        for tr in soup.find_all('tr'):
+            td = tr.find_all('td')
+            if td.__len__() == 7:
+                id = S.__len__()
+                S[id] = parse_ss(td[6].find('a', "dl1")['href'].split('//')[2])
+                S[id]['l'] = td[0].text
+    except:
+        print "获取 " + url + " 失败"
 
-if not S:
-    sys.exit("列表为空")
-for i, j in S.items():
-    # print 'ID:%-3d地址:%-18s端口:%-6s密码:%-16s加密:%-s混淆:%-23s协议:%-23s' % (i, j['s'], j['p'], j['k'], j['m'], j['o'], j['O'])
-    print 'ID:%-4d地址:%-25s位置:%s' % (i, j['s'], j['l'].encode('utf-8'))
-id = raw_input("请输入id:")
-while not id.isdigit() or int(id) not in S:
-    id = raw_input("请输入正确的id:")
-id = int(id)
-c = ssr_path + " -s %s -p %s -k %s -m %s -o %s -O %s -d restart -q --pid-file /tmp/shadowsocksr.pid --log-file /tmp/shadowsocksr.log> /dev/null 2>&1" % (S[id]['s'],S[id]['p'],S[id]['k'],S[id]['m'],S[id]['o'],S[id]['O'])
-if os.system(c) == 0:
-    print "已开启服务: ID:%-4d地址:%-30s位置:%s" % (id, S[id]['s'], S[id]['l'].encode('utf-8'))
+# 检查缓存文件如果在1小时之内就加载缓存
+temp_file = 'temp_ss.json'
+if not os.path.exists(temp_file) or time.time() - os.path.getmtime(temp_file) > 3600:
+    print "获取中......"
+    doub()
+    # Alvin9999()
+    open(temp_file, 'w').write(json.dumps(S)) if S else True
 else:
-    print "程序出错"
+    print "获取缓存数据 创建于%d分钟之前" % ((time.time() - os.path.getmtime(temp_file)) / 60)
+    S = {int(i): j for i, j in json.loads(open(temp_file, 'r').read()).items()}
+True if S else sys.exit("未获取到数据")
+
+for i, j in S.items():
+    print 'ID:' + str(i).ljust(4) + u'地址:' + j['s'].ljust(35) + u'位置:' + j['l']
+id = -1
+while id not in S:
+    id = int(raw_input("请输入正确的id:"))
+c = ssr_path + " -s %s -p %s -k %s -m %s -o %s -O %s -d restart -q --pid-file /tmp/shadowsocksr.pid --log-file /tmp/shadowsocksr.log> /dev/null 2>&1" % (S[id]['s'],S[id]['p'],S[id]['k'],S[id]['m'],S[id]['o'],S[id]['O'])
+print u'已开启服务:ID:' + str(i).ljust(4) + u'地址:' + j['s'].ljust(35) + u'位置:' + j['l'] if os.system(c) == 0 else "程序出错"
