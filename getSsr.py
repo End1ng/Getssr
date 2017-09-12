c8f365d0cf60e818bd1648416fc42d80817a58d3
Y <Y>
2017-09-12 14:38:44 +0800

    .

--- getSsr.py
+++ getSsr.py
@@ -12,52 +12,60 @@
 import requests, re, sys, os, signal, base64, json, time
 from bs4 import BeautifulSoup
 
+# 声明变量
 ssr_path = "/opt/shadowsocksr/shadowsocks/local.py"
 temp_path = os.path.expanduser('~') + "/.getSsr"
 True if os.path.exists(temp_path) else os.makedirs(temp_path)
 temp_file = temp_path + '/temp_config.json'
+stop_file = temp_path + '/stop.sh'
 S = {}
 
-use_old_data = True if '-n' in sys.argv or '-t' in sys.argv else False
+# 获取参数
+sys.exit(__doc__) if '-h' in sys.argv else False
+use_old_data = True if '-n' in sys.argv else False
 use_stop = True if '-t' in sys.argv else False
-print __doc__;sys.exit() if '-h' in sys.argv else False
 
+#定义函数
 def CtrlCHandler(signum, frame):
     sys.exit("\n再见!")
 signal.signal(signal.SIGINT, CtrlCHandler)
 
-True if os.path.exists(ssr_path) else sys.exit("未找到shadowsocksr 请安装:\n  sudo git clone https://github.com/Ni7eipr/shadowsocksr.git /opt/shadowsocksr\n或更改配置:\n  8 ssr_path = \"path\"")
-
-def add_padding(data):
+def parse_base64(data):
     return base64.b64decode(data + (data.__len__() % 4 + 1) * '=')
 
 def parse_ss(ss):
-    ss = add_padding(ss).split(':')
+    ss = parse_base64(ss).split(':')
     # ipv4 ssr链接
     if ss.__len__() == 6:
-        return {'s':ss[0],'p':ss[1],'O':ss[2],'m':ss[3],'o':ss[4],'k':add_padding(ss[5].split('/')[0])}
+        return {'s':ss[0],'p':ss[1],'O':ss[2],'m':ss[3],'o':ss[4],'k':parse_base64(ss[5].split('/')[0])}
     # ipv4 ss链接
     if ss.__len__() == 3:
         return {'s':ss[1].split('@')[1],'p':ss[2],'O':'origin','m':ss[0],'o':'plain','k':ss[1].split('@')[0]}
     # ipv6 链接
-    return {'s':':'.join(ss[:-5]),'p':ss[-5],'O':ss[-4],'m':ss[-3],'o':ss[-2],'k':add_padding(ss[-1].split('/')[0])}
+    return {'s':':'.join(ss[:-5]),'p':ss[-5],'O':ss[-4],'m':ss[-3],'o':ss[-2],'k':parse_base64(ss[-1].split('/')[0])}
 
-# def Alvin9999():
-#     url = "https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
-#     # comp_ogete_ss = re.compile("<p>服务器\d*(.*?)：([\w\.]+)\s*端口.*?(\d*)\s*密码.*?([\w\.-]*)\s*加密方式.*?([\w-]*)")
-#     comp_ogete_ssr = re.compile("<p>服务器\d+.+?([\w\.]+)\s*端口.+?(\d*)\s*密码.+?([\w\.-]*)\s*加密方式.+?([\w\.-]*)\s*SSR协议.+?协议.+?([\w\.-]*)\s*混淆.+?([\w\.-]*)\s*\（自建\）.*")
-#     try:
-#         res = requests.get(url).content
-#     except:
-#         print "获取 " + url + " 失败"
-#     html = re.findall("<p>服务器.*</p>", res)
-#     for i in html:
-#         # res = re.match(comp_ogete_ss, i)
-#         # if res:
-#         #     S[S.__len__()] = {'s':res.group(1), 'i':res.group(2), 'p':res.group(3), 'k':res.group(4), 'm':res.group(5)}
-#         res = re.match(comp_ogete_ssr, i)
-#         if res:
-#             S[S.__len__()] = {'s':res.group(1),'p':res.group(2),'k':res.group(3),'m':res.group(4),'O':res.group(5),'o':res.group(6),'l':''}
+True if os.path.exists(ssr_path) else sys.exit("未找到shadowsocksr 请安装:\n  sudo git clone https://github.com/Ni7eipr/shadowsocksr.git /opt/shadowsocksr\n或更改配置:\n  8 ssr_path = \"path\"")
+
+with open(stop_file, 'r') as f:
+    os.system(f.read());sys.exit('停止成功') if use_stop else False
+
+# 获取数据函数
+def Alvin9999():
+    url = "https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
+    # comp_ogete_ss = re.compile("<p>服务器\d*(.*?)：([\w\.]+)\s*端口.*?(\d*)\s*密码.*?([\w\.-]*)\s*加密方式.*?([\w-]*)")
+    comp_ogete_ssr = re.compile("<p>服务器\d+.+?([\w\.]+)\s*端口.+?(\d*)\s*密码.+?([\w\.-]*)\s*加密方式.+?([\w\.-]*)\s*SSR协议.+?协议.+?([\w\.-]*)\s*混淆.+?([\w\.-]*)\s*\（自建\）.*")
+    try:
+        res = requests.get(url).content
+    except:
+        print "获取 " + url + " 失败"
+    html = re.findall("<p>服务器.*</p>", res)
+    for i in html:
+        # res = re.match(comp_ogete_ss, i)
+        # if res:
+        #     S[S.__len__()] = {'s':res.group(1), 'i':res.group(2), 'p':res.group(3), 'k':res.group(4), 'm':res.group(5)}
+        res = re.match(comp_ogete_ssr, i)
+        if res:
+            S[S.__len__()] = {'s':res.group(1),'p':res.group(2),'k':res.group(3),'m':res.group(4),'O':res.group(5),'o':res.group(6),'l':''}
 
 def doub():
     url = "https://doub.bid/sszhfx"
@@ -81,16 +89,17 @@ def doub():
     except:
         print "获取 " + url + " 失败"
 
-# 检查缓存文件如果在1小时之内就加载缓存
+# 检查缓存文件
 if not os.path.exists(temp_file) or not use_old_data or time.time() - os.path.getmtime(temp_file) > 3600 * 12:
-        print "获取中......"
-        doub()
-        # Alvin9999()
-        f = open(temp_file, 'w').write(json.dumps(S)) if S else True
+    print "获取中......"
+
+    doub()
+    # Alvin9999()
 else:
     print "获取缓存数据 创建于%d分钟之前" % ((time.time() - os.path.getmtime(temp_file)) / 60)
     S = {int(i): j for i, j in json.loads(open(temp_file, 'r').read()).items()}
-True if S else sys.exit("未获取到数据")
+
+f = open(temp_file, 'w').write(json.dumps(S)) if S else sys.exit("未获取到数据")
 
 for i, j in S.items():
     print 'ID:' + str(i).ljust(4) + u'地址:' + j['s'].ljust(35) + u'位置:' + j['l']
@@ -100,7 +109,9 @@ while id not in S:
     id = raw_input("请输入id:")
     id = int(id) if id.isdigit() else -1
 
-op = ['stop', u'停止'] if use_stop else ['restart', u'开启']
-
-c = ssr_path + " -s %s -p %s -k %s -m %s -o %s -O %s -d %s -q --pid-file %s/shadowsocksr.pid --log-file %s/shadowsocksr.log> /dev/null 2>&1" % (S[id]['s'],S[id]['p'],S[id]['k'],S[id]['m'],S[id]['o'],S[id]['O'],op[0],temp_path,temp_path)
-print u'已' + op[1] + u'服务:ID:' + str(id).ljust(4) + u'地址:' + S[id]['s'].ljust(35) + u'位置:' + S[id]['l'] if os.system(c) == 0 else "程序出错"
+c = ssr_path + " -s %s -p %s -k %s -m %s -o %s -O %s -d %s -q --pid-file %s/shadowsocksr.pid --log-file %s/shadowsocksr.log> /dev/null 2>&1" % (S[id]['s'],S[id]['p'],S[id]['k'],S[id]['m'],S[id]['o'],S[id]['O'],'%s',temp_path,temp_path)
+c_start = c % 'restart'
+c_stop = c % 'stop'
+with open(stop_file, 'w') as f:
+    f.write(c_stop)
+print u'已开启服务:ID:' + str(id).ljust(4) + u'地址:' + S[id]['s'].ljust(35) + u'位置:' + S[id]['l'] if os.system(c_start) == 0 else "程序出错"
