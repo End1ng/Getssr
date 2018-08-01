5917b84b8881ab181930110f2c3986dadfda43cf
Ni7eipr <Ni7eipr+github@gmail.com>
2018-08-01 13:12:52 +0800

    。

--- getSsr.py
+++ getSsr.py
@@ -8,6 +8,7 @@
 -c 使用缓存列表
 -f 强制刷新
 -t 停止已开启服务
+-s ssr://*** 直接使用当前地址
 """
 
 import requests, re, sys, os, signal, base64, json, time
@@ -21,12 +22,15 @@ temp_file = temp_path + '/temp_config.json'
 stop_file = temp_path + '/stop.sh'
 True if os.path.exists(stop_file) else open(stop_file, 'w')
 S = {}
+requests.packages.urllib3.disable_warnings()
+cid = -1
 
 # 获取参数
 sys.exit(__doc__) if '-h' in sys.argv else False
 use_old_data = True if '-c' in sys.argv else False
 use_new_data = True if '-f' in sys.argv else False
 use_stop = True if '-t' in sys.argv else False
+drect = True if '-s' in sys.argv else False
 
 #定义函数
 def CtrlCHandler(signum, frame):
@@ -37,7 +41,7 @@ def parse_base64(data):
     return base64.b64decode(data + (data.__len__() % 4 + 1) * '=')
 
 def parse_ss(ss):
-    ss = parse_base64(ss).split(':')
+    ss = parse_base64(ss.split('//')[-1]).split(':')
     # ipv4 ssr链接
     if ss.__len__() == 6:
         return {'s':ss[0],'p':ss[1],'O':ss[2],'m':ss[3],'o':ss[4],'k':parse_base64(ss[5].split('/')[0])}
@@ -47,48 +51,12 @@ def parse_ss(ss):
     # ipv6 链接
     return {'s':':'.join(ss[:-5]),'p':ss[-5],'O':ss[-4],'m':ss[-3],'o':ss[-2],'k':parse_base64(ss[-1].split('/')[0])}
 
-# 获取数据函数
-def Alvin9999():
-    url = "https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
-    # comp_ogete_ss = re.compile("<p>服务器\d*(.*?)：([\w\.]+)\s*端口.*?(\d*)\s*密码.*?([\w\.-]*)\s*加密方式.*?([\w-]*)")
-    comp_ogete_ssr = re.compile("<p>服务器\d+.+?([\w\.]+)\s*端口.+?(\d*)\s*密码.+?([\w\.-]*)\s*加密方式.+?([\w\.-]*)\s*SSR协议.+?协议.+?([\w\.-]*)\s*混淆.+?([\w\.-]*)\s*\（自建\）.*")
-    try:
-        res = requests.get(url).content
-    except:
-        print "获取 " + url + " 失败"
-    html = re.findall("<p>服务器.*</p>", res)
-    for i in html:
-        # res = re.match(comp_ogete_ss, i)
-        # if res:
-        #     S[S.__len__()] = {'s':res.group(1), 'i':res.group(2), 'p':res.group(3), 'k':res.group(4), 'm':res.group(5)}
-        res = re.match(comp_ogete_ssr, i)
-        if res:
-            S[S.__len__()] = {'s':res.group(1),'p':res.group(2),'k':res.group(3),'m':res.group(4),'O':res.group(5),'o':res.group(6),'l':''}
-
-def doub():
-    url = "https://doub.bid/sszhfx"
-    session = requests.Session()
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0'
-    }
-    res = session.get(url, headers=headers)
-    print re.search('<p id="problem">(.*)</p>', res.text).group(1)
-    input_text = raw_input("请输入答案:")
-    while not input_text:
-        input_text = raw_input("请输入答案:")
-    session.post(re.search('<form action="(.*)" method="post">', res.text).group(1), data={'post_password':input_text}, headers=headers)
-    res = session.get(url,  headers=headers).content
-    soup = BeautifulSoup(res, 'lxml')
-    for tr in soup.find_all('tr'):
-        td = tr.find_all('td')
-        if td.__len__() == 7:
-            try:
-                id = S.__len__()
-                S[id] = parse_ss(td[6].find('a', "dl1")['href'].split('//')[2])
-                S[id]['l'] = td[0].text
-            except:
-                pass
-    print "获取 " + url + " 失败"
+def default():
+    with open('data.txt') as f:
+        for i in f:
+            sid = S.__len__()
+            S[sid] = parse_ss(i.strip())
+            S[sid]['l'] = "self"
 
 True if os.path.exists(ssr_path) else sys.exit("未找到shadowsocksr 请安装:\n  sudo git clone https://github.com/Ni7eipr/shadowsocksr.git /opt/shadowsocksr\n或更改配置:\n  8 ssr_path = \"path\"")
 
@@ -104,20 +72,26 @@ else:
 
     doub()
     # Alvin9999()
+    default()
 
 f = open(temp_file, 'w').write(json.dumps(S)) if S else sys.exit("未获取到数据")
+
+if drect:
+    sid = S.__len__()
+    S[sid] = parse_ss(sys.argv[2])
+    S[sid]['l'] = "drect"
+    cid = sid
 
 for i, j in S.items():
     print 'ID:' + str(i).ljust(4) + u'地址:' + j['s'].ljust(35) + u'位置:' + j['l']
 
-id = -1
-while id not in S:
-    id = raw_input("请输入id:")
-    id = int(id) if id.isdigit() else -1
+while cid not in S:
+    cid = raw_input("请输入id:")
+    cid = int(cid) if cid.isdigit() else -1
 
-c = ssr_path + " -s %s -p %s -k %s -m %s -o %s -O %s -d %s -q --pid-file %s/shadowsocksr.pid --log-file %s/shadowsocksr.log > /dev/null 2>&1" % (S[id]['s'],S[id]['p'],S[id]['k'],S[id]['m'],S[id]['o'],S[id]['O'],'%s',temp_path,temp_path)
+c = ssr_path + " -s %s -p %s -k %s -m %s -o %s -O %s -d %s -q --pid-file %s/shadowsocksr.pid --log-file %s/shadowsocksr.log > /dev/null 2>&1" % (S[cid]['s'],S[cid]['p'],S[cid]['k'],S[cid]['m'],S[cid]['o'],S[cid]['O'],'%s',temp_path,temp_path)
 c_start = c % 'restart'
 c_stop = c % 'stop'
 f_c = open(stop_file, 'w')
 f_c.write(c_stop)
-print u'已开启服务:ID:' + str(id).ljust(4) + u'地址:' + S[id]['s'].ljust(35) + u'位置:' + S[id]['l'] if os.system(c_start) == 0 else "程序出错"
+print u'已开启服务:ID:' + str(cid).ljust(4) + u'地址:' + S[cid]['s'].ljust(35) + u'位置:' + S[cid]['l'] if os.system(c_start) == 0 else "程序出错"
